d7b98b86b6e0a59b37b734c449e765146e005bbf
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-05-02 13:50:53 +1200

    setting up

--- routes.py
+++ routes.py
@@ -9,5 +9,16 @@ app = Flask(__name__)
 def sign_in():
     return render_template("sign_in.html")
 
+
+@app.route('/home') #home page
+def home():
+    return render_template("home.html")
+
+
+@app.route('/allbooks')
+def all_books():
+    return render_template("all_books.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
