99f6c136de2990e3b5d8c956647f43319f99afab
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-05-10 14:12:13 +1200

    continue as guest redirect

--- routes.py
+++ routes.py
@@ -4,24 +4,24 @@ import sqlite3
 
 app = Flask(__name__)
 
-
-@app.route('/', methods = ['GET', 'POST']) #sign in page
+# for signin i used the tutorial https://realpython.com/introduction-to-flask-part-2-creating-a-login-page/
+@app.route("/", methods = ["GET", "POST"]) #sign in page
 def sign_in():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Incorrect username or password. Please try again.'
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "admin": #check if input is correct
+            error = "Incorrect username or password. Please try again." #error message if input is wrong
         else:
-            return redirect(url_for('home'))
+            return redirect(url_for("home")) #if correct, takes you to home page
     return render_template("sign_in.html", error=error)
 
 
-@app.route('/home') #home page
+@app.route("/home") #home page
 def home():
     return render_template("home.html")
 
 
-@app.route('/allbooks')
+@app.route("/allbooks")
 def all_books():
     conn = sqlite3.connect("bookshelf.db")
     cur = conn.cursor()
