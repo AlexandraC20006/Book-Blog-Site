896b92782fc73dcfa924e8219f4b701b5666a359
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-05-25 12:03:23 +1200

    use flask sessions to tell site you're signed in

--- routes.py
+++ routes.py
@@ -1,30 +1,51 @@
+
+###############################################################
+##                                                           ##
+##   NOTE: School computers wipe every time you log off!     ##
+##         In terminal, write "pip install flask-session"    ##
+##         every time you log back on                        ##
+##                                                           ##
+###############################################################
+
+
 from flask import Flask, render_template, request, session, redirect, url_for
+from flask_session import Session
 import sqlite3
 
 
 app = Flask(__name__)
-logged_in=False
+
+
+# Configure the app to use sessions
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
 
 # for signin i used the tutorial https://realpython.com/introduction-to-flask-part-2-creating-a-login-page/
 @app.route("/", methods = ["GET", "POST"]) #sign in page
 def sign_in():
+    # user_id = session.get("user_id") # Retrieve a value from the session
+
     error = None
     if request.method == "POST":
-        if request.form["username"] != "admin" or request.form["password"] != "admin": #check if input is correct
+        username = request.form["username"]
+        password = request.form["password"]
+        if username != "admin" or password != "admin": #check if input is correct
             error = "Incorrect username or password. Please try again." #error message if input is wrong
         else:
-            global logged_in
-            logged_in=True
+            session["username"] = username # Store a value in the session
+            session["logged_in"] = True # Store a value in the session
             return redirect(url_for("home")) #if correct, takes you to home page
     return render_template("sign_in.html", error=error)
 
 
 @app.route("/home") #home page
 def home():
-    if logged_in == True:
-        return render_template("home_si.html")
+    if session.get("logged_in") == True:
+        return render_template("home_si.html") #takes you to signed in page
     else:
-        return render_template("home_so.html")
+        return render_template("home_so.html") #takes you to signed out page
 
 
 
